23c1c75ca433b43bfd43af6c615e678d3daf1556
lkadiryalcinl <kaadir.yaalcin@gmail.com>
2023-03-09 17:34:14 +0300

    Project loading...

---
+++ src/Router.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Home from './Pages/Home';
+import Detail from './Pages/Detail';
+import FavouritedJobs from './Pages/FavouritedJobs';
+
+const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function Jobs () {
+    return(
+        <Stack.Navigator initialRouteName='Home'>
+            <Stack.Screen 
+                name="Home" 
+                component={Home}
+                options={{headerShown:false}}
+            />
+            <Stack.Screen 
+                name="Detail" 
+                component={Detail}
+                options={{headerTintColor:'#c31432',headerTitleAlign:'center'}}
+            />
+        </Stack.Navigator>
+    )
+}
+
+function FavouriteJobs() {
+    return(
+        <Stack.Navigator>
+            <Stack.Screen
+                name='FavJobs'
+               component={FavouritedJobs}
+                options={{headerShown:false}}
+            />
+        </Stack.Navigator>
+    )
+    
+}
+
+export default() => {
+    return(
+        <NavigationContainer>
+            <Drawer.Navigator>
+                <Drawer.Screen
+                    name='Jobs'
+                    component={Jobs}
+                    options={
+                        {
+                            headerShown:false,
+                            drawerActiveTintColor:'#c31432'
+                        }
+                    }
+                />
+
+                <Drawer.Screen
+                    name='FavouritedJobs'
+                    component={FavouriteJobs}
+                    options={
+                        {
+                            headerShown:false,
+                            drawerActiveTintColor:'#c31432'
+                            
+                        }
+                    }
+                />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    ) 
+}
